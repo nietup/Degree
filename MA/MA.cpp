16e1ec68adb601081f86db62de7bbf9503cc55e3
nietup <22.jakub@gmail.com>
2017-04-10 19:50:50 +0200

    problem visualisation

--- MA/MA.cpp
+++ MA/MA.cpp
@@ -32,7 +32,7 @@ int main() {
 }
 
 void DetectionTest() {
-    cout << "In this testing case we hardcode triangle model and test classification";
+    //cout << "In this testing case we hardcode triangle model and test classification";
 
     srand((uint)time(NULL));
 
@@ -65,6 +65,15 @@ void DetectionTest() {
     ShapesDetectorInterface::Ptr detector(new ElsDetector);
     detector->run(image);
 
+    //save image
+    /*string outFile = inFile + ".svg";
+    SvgWriterInterface::Ptr svg(new ElsdSvgWriter);
+    svg->setImageSize(image->xsize(), image->ysize());
+    svg->addLineSegments(detector->getLineSegments().begin(), detector->getLineSegments().end());
+    ofstream ofs(outFile, ofstream::out);
+    ofs << *svg;
+    ofs.close();*/
+
     vector<LineSegment> ls = detector->getLineSegments();
 
     //matcher seems to work for now, lets focus on
@@ -72,19 +81,59 @@ void DetectionTest() {
     int * match = mat.Match();
     for (int i = 0; i < 3; i++)
         cout << "\n" << match[i];*/
-    TestScore(ls[0],ls[1]);
+    TestScore(ls[2],ls[3]);
 }
 
+/*
+ anlge a between v and u
+
+ cos a    = dot(v,u) / (len(v)*len(u))
+ dot(v,u) = v.x*u.x + v.y*u.y
+ len(v)   = sqrt(v.x^2 + v.y^2)
+*/
 int TestScore(LineSegment & a, LineSegment & b) {
-    double x1 = a.startPoint[0],
+    /*double x1 = a.startPoint[0],
            y1 = a.startPoint[1],
            x2 = a.endPoint[0],
-           y2 = a.endPoint[1];
-    cout << "\nfirst we will count line length using point coords"
+           y2 = a.endPoint[1],
+           bx1 = b.startPoint[0],
+           by1 = b.startPoint[1],
+           bx2 = b.endPoint[0],
+           by2 = b.endPoint[1],
+           lenA = sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)),
+           lenB = sqrt((bx2-bx1)*(bx2-bx1)+(by2-by1)*(by2-by1)),
+           dot = (x2-x1)*(bx2-bx1) + (y2-y1)*(by2-by1),
+           cos = dot/(lenA*lenB);*/
+
+    /*cout << "\nfirst we will count line length using point coords"
          << "\nx1: " << x1 << " y1: " << y1
          << "\nx2: " << x2 << " y2: " << y2
-         << "\nsqrt((x2-x1)^2+(y2-y1)^2): " << sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
-    cout << "\n\nnow lets calculate angle between two line segments";
+         << "\nsqrt((x2-x1)^2+(y2-y1)^2): " << lenA
+         << "\n\nnow lets calculate cos angle between two line segments"
+         << "\nbx1: " << bx1 << " by1: " << by1
+         << "\nbx2: " << bx2 << " by2: " << by2
+         << "\nlenB: " << lenB
+         << "\ndot(a, b): " << dot
+         << "\ncos: " << cos;*/
+
+
+
+    double by1 = b.startPoint[1];
+    double by2 = b.endPoint[1];
+
+    double hard1 = 361.538;
+    double hard2 = 361.53;
+
+    cout << "\nb.startPoint[1]:\t\t\t\t\t"                << b.startPoint[1]
+         << "\nb.endPoint[1]:\t\t\t\t\t\t"                << b.endPoint[1]
+         << "\nb.startPoint[1] - b.endPoint[1]:\t"        << b.startPoint[1] - b.endPoint[1]
+         << "\ndouble by1 = b.startPoint[1]:\t\t"         << by1
+         << "\ndouble by2 = b.endPoint[1]:\t\t\t"         << by2
+         << "\nby1-by2:\t\t\t\t\t\t\t"                    << by1-by2
+         << "\ndouble hard1 = 361.538:\t\t\t\t"           << hard1
+         << "\ndouble hard2 = 361.53:\t\t\t\t"            << hard2
+         << "\nhard1 - hard2:\t\t\t\t\t\t"                << hard1 - hard2;
+
 
     return 0;
 }

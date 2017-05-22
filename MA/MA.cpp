33a53536ebbdf38cb4abdce3a409d0805ab4f9e0
nietup <22.jakub@gmail.com>
2017-05-22 11:50:30 +0200

    cleanup for model generation

--- MA/MA.cpp
+++ MA/MA.cpp
@@ -12,7 +12,11 @@
 using namespace std;
 using namespace elsd;
 
-void TestDetection() {
+unique_ptr<SModel> GenerateModel(vector<vector<weak_ptr<LineWrap>>> samples) {
+    return unique_ptr<SModel>();
+}
+
+void TestGeneration() {
     //function scoring size match
     //* returns double in range [0, 1], where 0 is the best match
     auto sizeMatch = make_shared<Constraint>([](const LineWrap &a,
@@ -149,204 +153,45 @@ void TestDetection() {
         return a1 / (a1 + 300.0);
     });
 
-
-    auto model = SModel{};
-
-    /*
-     * foremny Triangle model
-     */
-
-    /*auto a1 = make_shared<Atom>(Atom{"1"});
-    auto a2 = make_shared<Atom>(Atom{"2"});
-    auto a3 = make_shared<Atom>(Atom{"3"});
-
-    auto v = vector<weak_ptr<Constraint>>{
-        weak_ptr<Constraint>(angle60),
-        weak_ptr<Constraint>(adjacent),
-        weak_ptr<Constraint>(sizeMatch)
-    };
-
-    auto p1 = make_shared<Part>(Part{{a1, a2}, v});
-    auto p2 = make_shared<Part>(Part{{a1, a3}, v});
-    auto p3 = make_shared<Part>(Part{{a2, a3}, v});
-
-    a1.get()->involved.push_back(weak_ptr<Part>(p1));
-    a1.get()->involved.push_back(weak_ptr<Part>(p2));
-    a2.get()->involved.push_back(weak_ptr<Part>(p1));
-    a2.get()->involved.push_back(weak_ptr<Part>(p3));
-    a3.get()->involved.push_back(weak_ptr<Part>(p2));
-    a3.get()->involved.push_back(weak_ptr<Part>(p3));
-
-    model.atoms.push_back(a1);
-    model.atoms.push_back(a2);
-    model.atoms.push_back(a3);
-
-    model.parts.push_back(p1);
-    model.parts.push_back(p2);
-    model.parts.push_back(p3);*/
-
-    /*
-     * general Triangle model
-     */
-
-    auto a1 = make_shared<Atom>(Atom{"1"});
-    auto a2 = make_shared<Atom>(Atom{"2"});
-    auto a3 = make_shared<Atom>(Atom{"3"});
-
-    auto v = vector<weak_ptr<Constraint>>{
-        weak_ptr<Constraint>(adjacent),
-        weak_ptr<Constraint>(notParallel)
-    };
-
-    auto vs = vector<weak_ptr<Constraint>>{
-        weak_ptr<Constraint>(adjacentStart),
-        weak_ptr<Constraint>(notParallel)
-    };
-
-    auto ve = vector<weak_ptr<Constraint>>{
-        weak_ptr<Constraint>(adjacentEnd),
-        weak_ptr<Constraint>(notParallel)
-    };
-
-    auto p1 = make_shared<Part>(Part{{a1, a2}, vs});
-    auto p2 = make_shared<Part>(Part{{a1, a3}, ve});
-    auto p3 = make_shared<Part>(Part{{a2, a3}, v});
-
-    a1.get()->involved.push_back(weak_ptr<Part>(p1));
-    a1.get()->involved.push_back(weak_ptr<Part>(p2));
-    a2.get()->involved.push_back(weak_ptr<Part>(p1));
-    a2.get()->involved.push_back(weak_ptr<Part>(p3));
-    a3.get()->involved.push_back(weak_ptr<Part>(p2));
-    a3.get()->involved.push_back(weak_ptr<Part>(p3));
-
-    model.atoms.push_back(a1);
-    model.atoms.push_back(a2);
-    model.atoms.push_back(a3);
-
-    model.parts.push_back(p1);
-    model.parts.push_back(p2);
-    model.parts.push_back(p3);
-
-    /*
-     * square model
-     */
-
-    /*auto a1 = make_shared<Atom>(Atom{"1"});
-    auto a2 = make_shared<Atom>(Atom{"2"});
-    auto a3 = make_shared<Atom>(Atom{"3"});
-    auto a4 = make_shared<Atom>(Atom{"4"});
-
-    auto closeEdges = vector<weak_ptr<Constraint>>{
-        weak_ptr<Constraint>(adjacent),
-        weak_ptr<Constraint>(sizeMatch),
-        weak_ptr<Constraint>(perpendicular)
-    };
-
-    auto farEdges = vector<weak_ptr<Constraint>>{
-        weak_ptr<Constraint>(parallel),
-        weak_ptr<Constraint>(sizeMatch),
-        weak_ptr<Constraint>(far)
-    };
-
-    auto p1 = make_shared<Part>(Part{{a1, a2}, closeEdges});
-    auto p2 = make_shared<Part>(Part{{a1, a3}, farEdges});
-    auto p3 = make_shared<Part>(Part{{a1, a4}, closeEdges});
-    auto p4 = make_shared<Part>(Part{{a2, a3}, closeEdges});
-    auto p5 = make_shared<Part>(Part{{a2, a4}, farEdges});
-    auto p6 = make_shared<Part>(Part{{a3, a4}, closeEdges});
-
-    a1.get()->involved.push_back(weak_ptr<Part>(p1));
-    a1.get()->involved.push_back(weak_ptr<Part>(p2));
-    a1.get()->involved.push_back(weak_ptr<Part>(p3));
-    a2.get()->involved.push_back(weak_ptr<Part>(p1));
-    a2.get()->involved.push_back(weak_ptr<Part>(p4));
-    a2.get()->involved.push_back(weak_ptr<Part>(p5));
-    a3.get()->involved.push_back(weak_ptr<Part>(p2));
-    a3.get()->involved.push_back(weak_ptr<Part>(p4));
-    a3.get()->involved.push_back(weak_ptr<Part>(p6));
-    a4.get()->involved.push_back(weak_ptr<Part>(p3));
-    a4.get()->involved.push_back(weak_ptr<Part>(p5));
-    a4.get()->involved.push_back(weak_ptr<Part>(p6));
-
-    model.atoms.push_back(a1);
-    model.atoms.push_back(a2);
-    model.atoms.push_back(a3);
-    model.atoms.push_back(a4);
-
-    model.parts.push_back(p1);
-    model.parts.push_back(p2);
-    model.parts.push_back(p3);
-    model.parts.push_back(p4);
-    model.parts.push_back(p5);
-    model.parts.push_back(p6);*/
-
     /*
      * ELSD image processing
      * detect segments and save them in separate image for testing purposes
      */
 
-    cout << "file name: ";
-    auto inFile = string{};
-    cin >> inFile;
-    inFile = "./" + inFile;
-    ImageInterface::Ptr image(new ElsdPgmFileReader(inFile));
-    ShapesDetectorInterface::Ptr detector(new ElsDetector);
-    detector->run(image);
+    auto samples = vector<vector<weak_ptr<LineWrap>>>{};
 
-    string outFile = inFile + ".svg";
-    SvgWriterInterface::Ptr svg(new ElsdSvgWriter);
-    svg->setImageSize(image->xsize(), image->ysize());
-    const vector<LineSegment> &lines = detector->getLineSegments();
-    svg->addLineSegments(lines.begin(), lines.end());
-    ofstream ofs(outFile, ofstream::out);
-    ofs << *svg
-        << "</svg>";
-    ofs.close();
+    auto sampleFiles = vector<string>{/*"l1.PGM", "l2.PGM", "l3.PGM", "l4.PGM"*/};
+    for (auto & sampleFile : sampleFiles) {
+        auto inFile = sampleFile;
+        inFile = "./" + inFile;
+        ImageInterface::Ptr image(new ElsdPgmFileReader(inFile));
+        ShapesDetectorInterface::Ptr detector(new ElsDetector);
+        detector->run(image);
 
-    /*
-     * Create vector of wrapped segments
-     * and apply it to the matcher
-     */
+        string outFile = inFile + ".svg";
+        SvgWriterInterface::Ptr svg(new ElsdSvgWriter);
+        svg->setImageSize(image->xsize(), image->ysize());
+        const vector<LineSegment> &lines = detector->getLineSegments();
+        svg->addLineSegments(lines.begin(), lines.end());
+        ofstream ofs(outFile, ofstream::out);
+        ofs << *svg
+            << "</svg>";
+        ofs.close();
 
-    //TODO fix so there is no copy
-    auto segsShared = vector<shared_ptr<LineWrap>>();
-    for (const auto &line : lines)
-        segsShared.push_back(make_shared<LineWrap>(line));
+        //Create vector of wrapped segments
+        //TODO fix so there is no copy
+        auto segsShared = vector<shared_ptr<LineWrap>>();
+        for (const auto &line : lines)
+            segsShared.push_back(make_shared<LineWrap>(line));
 
-    //square debug part
-    /*segsShared.push_back(make_shared<LineWrap>(
-        LineWrap{{351.0,146.0},{351.0,351.0}}
-    ));*/
+        auto segments = vector<weak_ptr<LineWrap>>();
+        for (const auto &line : segsShared)
+            segments.push_back(weak_ptr<LineWrap>(line));
 
-    auto segments = vector<weak_ptr<LineWrap>>();
-    for (const auto &line : segsShared)
-        segments.push_back(weak_ptr<LineWrap>(line));
-
-
-    if (Match(model, segments))
-        for (auto &a : model.atoms) {
-            cout << a->name << " <-> ("
-                 << a.get()->asignment.lock().get()->start.first << ", "
-                 << a.get()->asignment.lock().get()->start.second << ") ("
-                 << a.get()->asignment.lock().get()->end.first << ", "
-                 << a.get()->asignment.lock().get()->end.second << ")"
-                 << endl;
-
-            auto detection = vector<LineSegment>();
-            for (auto &a : model.atoms)
-                detection.push_back(a->asignment.lock()->GetLineSegment());
-
-            string detectionFile = inFile + ".detection.svg";
-            SvgWriterInterface::Ptr svg2(new ElsdSvgWriter);
-            svg2->setImageSize(image->xsize(), image->ysize());
-            svg2->addLineSegments(detection.begin(), detection.end());
-            ofs.open(detectionFile);
-            ofs << *svg2 << "</svg>";
-            ofs.close();
-        }
-    else {
-        cout << "Non match" << endl;
+        samples.push_back(segments);
     }
+
+    auto GeneratedModel = GenerateModel(samples);
 }
 
 int main() {
@@ -359,7 +204,7 @@ int main() {
 		SM.Render();
 	}*/
 
-    TestDetection();
+    TestGeneration();
 
 	return 0;
 }

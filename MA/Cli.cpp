0e1984c105c6509da7d9a15f3dd0f7a7338f1f4e
nietup <22.jakub@gmail.com>
2017-07-14 13:31:38 +0200

    saving model

--- MA/Cli.cpp
+++ MA/Cli.cpp
@@ -5,215 +5,6 @@
 #include "Cli.h"
 
 //-----------------------------------------------------------------------------
-void TestGeneration() {
-    //function scoring size match
-    //* returns double in range [0, 1], where 0 is the best match
-    auto sizeMatch = make_shared<Constraint>([](const LineWrap &a,
-                                                const LineWrap &b) {
-        double lenA = a.Length(),
-            lenB = b.Length();
-        /*cout << "size match: "
-             << (lenA < lenB ? 1 - (lenA / lenB) : 1 - (lenB / lenA))
-             << endl;*/
-        return lenA < lenB ? 1 - (lenA / lenB) : 1 - (lenB / lenA);
-    });
-
-    //function scoring perpendicularity between lines
-    //* returns double in range [0, 1], where 0 is the best match
-    auto perpendicular = make_shared<Constraint>([](const LineWrap &a,
-                                                    const LineWrap &b) {
-        return abs(a.GetCos(b));
-    });
-
-    //function scoring parallelity between lines
-    //* returns double in range [0, 1], where 0 is the best match
-    auto parallel = make_shared<Constraint>([](const LineWrap &a,
-                                               const LineWrap &b) {
-        return 1.0 - abs(a.GetCos(b));
-    });
-
-    auto notParallel = make_shared<Constraint>([](const LineWrap &a,
-                                                  const LineWrap &b) {
-        return 0.9 < abs(a.GetCos(b)) ? 1.0 : 0.0;
-    });
-
-    //function scoring closeness to 60 degrees between lines
-    //* returns double in range [0, 1], where 0 is the best match
-    auto angle60 = make_shared<Constraint>([](const LineWrap &a,
-                                              const LineWrap &b) {
-        //cout << "angle: " << (2*abs(0.5 - abs(a.GetCos(b))))
-        /* << endl
-        << " ax1: " << a.start.first
-
-        << " | ay1: " << a.start.second
-        << endl
-        << " ax2: " << a.end.first
-
-        << " | ay2: " << a.end.second
-        << endl << endl
-        << " bx1: " << b.start.first
-
-        << " | by1: " << b.start.second
-        << endl
-        << " bx2: " << b.end.first
-
-        << " | by2: " << b.end.second
-        << endl << endl
-
-        << "cos ab: " << a.GetCos(b)*/
-        //   << endl;
-
-        return 2 * abs(0.5 - abs(a.GetCos(b)));
-    });
-
-    //function scoring adjacency between lines
-    //* returns double in range [0, 1], where 0 is the best match
-    auto adjacent = make_shared<Constraint>([](const LineWrap &a,
-                                               const LineWrap &b) {
-        double d = a.Distance(b);
-        /*cout /*<< " ("
-             << a.start.first << ", "
-             << a.start.second << ")("
-             << a.end.first << ", "
-             << a.end.second << ")"
-             << " ("
-             << b.start.first << ", "
-             << b.start.second << ")("
-             << b.end.first << ", "
-             << b.end.second << ") "*/
-        //<< "adjacent: " << (d / (d + 1000))
-        //<< endl;
-        return d / (d + 300);
-    });
-
-    //function opposite to adjacent
-    //* returns double in range [0, inf), where inf is touching
-    auto far = make_shared<Constraint>([](const LineWrap & a,
-                                          const LineWrap & b) {
-        return 10.0 / a.Distance(b);
-    });
-
-    //function scoring adjacency between start of line a and line b
-    //* returns double in range [0, 1], where 0 is the best match
-    auto adjacentStart = make_shared<Constraint>([](const LineWrap & a,
-                                                    const LineWrap & b) {
-        auto d = b.Distance(a.start);
-        /*cout << "start\na: " << a.start.first << " " << a.start.second
-             << "  "  << a.end.first << " " << a.end.second << endl
-             << "b: " << b.start.first << " " << b.start.second
-             << "  "  << b.end.first << " " << b.end.second << endl
-             << "res: " << d / (d + 300.0) << endl;*/
-        return d / (d+300.0);
-    });
-
-    //function scoring adjacency between end  of line a and line b
-    //* returns double in range [0, 1], where 0 is the best match
-    auto adjacentEnd = make_shared<Constraint>([](const LineWrap & a,
-                                                  const LineWrap & b) {
-        auto d = b.Distance(a.end);
-        /*cout << "end\na: " << a.start.first << " " << a.start.second
-             << "  "  << a.end.first << " " << a.end.second << endl
-             << "b: " << b.start.first << " " << b.start.second
-             << "  "  << b.end.first << " " << b.end.second << endl
-             << "res: " << d / (d + 300.0) << endl;*/
-        return d / (d+300.0);
-    });
-
-    //function scoring adjacency between vertices of line a b
-    //* returns double in range [0, 1], where 0 is the best match
-    auto vertexAdjacent = make_shared<Constraint>([](const LineWrap & a,
-                                                     const LineWrap & b) {
-        auto a1 = b.Distance(a.start);
-        auto a2 = b.Distance(a.end);
-        a1 = a1 < a2 ? a1 : a2;
-
-        auto b1 = a.Distance(b.start);
-        auto b2 = a.Distance(b.end);
-        b1 = b1 < b2 ? b1 : b2;
-
-        a1 = a1 < b1 ? a1 : b1;
-
-        /*cout << "a: " << a.start.first << " " << a.start.second
-             << "  "  << a.end.first << " " << a.end.second << endl
-             << "b: " << b.start.first << " " << b.start.second
-             << "  "  << b.end.first << " " << b.end.second << endl
-             << "res: " << a1 / (a1 + 1000.0) << endl;*/
-
-        return a1 / (a1 + 300.0);
-    });
-
-    /*
-     * ELSD image processing
-     * detect segments and save them in separate image for testing purposes
-     */
-
-    auto samples = vector<vector<weak_ptr<LineWrap>>>{};
-
-    auto sampleFiles = vector<string>{/*"l1.PGM","l2.PGM","l3.PGM","l4.PGM"*/};
-    for (auto & sampleFile : sampleFiles) {
-        auto inFile = sampleFile;
-        inFile = "./" + inFile;
-        ImageInterface::Ptr image(new ElsdPgmFileReader(inFile));
-        ShapesDetectorInterface::Ptr detector(new ElsDetector);
-        detector->run(image);
-
-        string outFile = inFile + ".svg";
-        SvgWriterInterface::Ptr svg(new ElsdSvgWriter);
-        svg->setImageSize(image->xsize(), image->ysize());
-        const vector<LineSegment> &lines = detector->getLineSegments();
-        svg->addLineSegments(lines.begin(), lines.end());
-        ofstream ofs(outFile, ofstream::out);
-        ofs << *svg
-            << "</svg>";
-        ofs.close();
-
-        //Create vector of wrapped segments
-        auto segsShared = vector<shared_ptr<LineWrap>>();
-        for (const auto &line : lines)
-            segsShared.push_back(make_shared<LineWrap>(line));
-
-        auto segments = vector<weak_ptr<LineWrap>>();
-        for (const auto &line : segsShared)
-            segments.push_back(weak_ptr<LineWrap>(line));
-
-        samples.push_back(segments);
-    }
-
-    auto constraints = vector<shared_ptr<Constraint>>{adjacent, sizeMatch,
-                                                      parallel, perpendicular};
-
-    auto l00 = make_shared<LineWrap>(LineWrap{{0,0},{100,0}});
-    auto l01 = make_shared<LineWrap>(LineWrap{{100,100},{100,0}});
-    auto l02 = make_shared<LineWrap>(LineWrap{{0,0},{0,100}});
-    auto l03 = make_shared<LineWrap>(LineWrap{{0,100},{100,100}});
-    auto p0 = vector<weak_ptr<LineWrap>>{l00, l01, l02, l03};
-
-    auto l10 = make_shared<LineWrap>(LineWrap{{0,0},{500,0}});
-    auto l11 = make_shared<LineWrap>(LineWrap{{500,100},{500,0}});
-    auto l12 = make_shared<LineWrap>(LineWrap{{0,0},{0,100}});
-    auto l13 = make_shared<LineWrap>(LineWrap{{0,100},{500,100}});
-    auto p1 = vector<weak_ptr<LineWrap>>{l10, l11, l12, l13};
-
-    auto l20 = make_shared<LineWrap>(LineWrap{{0,0},{100,0}});
-    auto l21 = make_shared<LineWrap>(LineWrap{{120,97.9797},{100,0}});
-    auto l22 = make_shared<LineWrap>(LineWrap{{0,0},{20,97.9797}});
-    auto l23 = make_shared<LineWrap>(LineWrap{{20,97.9797},{120,97.9797}});
-    auto p2 = vector<weak_ptr<LineWrap>>{l20, l21, l22, l23};
-
-    auto l30 = make_shared<LineWrap>(LineWrap{{0,0},{100,0}});
-    auto l31 = make_shared<LineWrap>(LineWrap{{0,0},{0,100}});
-    auto l32 = make_shared<LineWrap>(LineWrap{{0,100},{100,0}});
-    auto l33 = make_shared<LineWrap>(LineWrap{{0,0},{50,50}});
-    auto n0 = vector<weak_ptr<LineWrap>>{l30, l31, l32, l33};
-
-    auto posSamples = vector<vector<weak_ptr<LineWrap>>>{p0, p1, p2};
-    auto negSamples = vector<vector<weak_ptr<LineWrap>>>{n0};
-
-    LearningSystem ls;
-    ls.GenerateModel(posSamples, negSamples, constraints);
-}
-
-//-----------------------------------------------------------------------------
 void TestMatching() {
     auto sizeMatch = [](const LineWrap &a,
                         const LineWrap &b) {
@@ -315,9 +106,6 @@ void TestMatching() {
         cout << "Non match" << endl;
     }
 }
-//-----------------------------------------------------------------------------
-//-----------------------------------------------------------------------------
-//-----------------------------------------------------------------------------
 
 
 //-----------------------------------------------------------------------------
@@ -406,8 +194,40 @@ void Cli::SelectTestingSamples() {
 
 //-----------------------------------------------------------------------------
 void Cli::SaveModel() {
+    string outFile = pathToModel;
+    ofstream ofs(outFile, ofstream::out);
+
+    auto edgesNo = model.edges.size();
+
+    for (auto & edge : model.edges) {
+        //get indices of vertices
+        auto index = 0;
+        auto verticesNo = model.vertices.size();
+        for (; index < verticesNo; index++) {
+            if (model.vertices[index] == edge->vertices.first.lock()) {
+                break;
+            }
+        }
+
+        ofs << index << ";";
+
+        index = 0;
+        for (; index < verticesNo; index++) {
+            if (model.vertices[index] == edge->vertices.second.lock()) {
+                break;
+            }
+        }
+
+        ofs << index << ";";
+
+        for (auto & cons : edge->constraints) {
+            ofs << cons << ";";
+        }
+    }
+
+    ofs.close();
+
     cout << "\nModel zapisano do:";
-    //TODO zapisac model
 }
 
 //-----------------------------------------------------------------------------
@@ -418,8 +238,162 @@ void Cli::Test() {
 
 //-----------------------------------------------------------------------------
 void Cli::Learn() {
-    TestGeneration();
-    //TODO
+    //function scoring size match
+    //* returns double in range [0, 1], where 0 is the best match
+    auto sizeMatch = make_shared<Constraint>([](const LineWrap &a,
+                                                const LineWrap &b) {
+        double lenA = a.Length(),
+            lenB = b.Length();
+        /*cout << "size match: "
+             << (lenA < lenB ? 1 - (lenA / lenB) : 1 - (lenB / lenA))
+             << endl;*/
+        return lenA < lenB ? 1 - (lenA / lenB) : 1 - (lenB / lenA);
+    });
+
+    //function scoring perpendicularity between lines
+    //* returns double in range [0, 1], where 0 is the best match
+    auto perpendicular = make_shared<Constraint>([](const LineWrap &a,
+                                                    const LineWrap &b) {
+        return abs(a.GetCos(b));
+    });
+
+    //function scoring parallelity between lines
+    //* returns double in range [0, 1], where 0 is the best match
+    auto parallel = make_shared<Constraint>([](const LineWrap &a,
+                                               const LineWrap &b) {
+        return 1.0 - abs(a.GetCos(b));
+    });
+
+    auto notParallel = make_shared<Constraint>([](const LineWrap &a,
+                                                  const LineWrap &b) {
+        return 0.9 < abs(a.GetCos(b)) ? 1.0 : 0.0;
+    });
+
+    //function scoring closeness to 60 degrees between lines
+    //* returns double in range [0, 1], where 0 is the best match
+    auto angle60 = make_shared<Constraint>([](const LineWrap &a,
+                                              const LineWrap &b) {
+
+        return 2 * abs(0.5 - abs(a.GetCos(b)));
+    });
+
+    //function scoring adjacency between lines
+    //* returns double in range [0, 1], where 0 is the best match
+    auto adjacent = make_shared<Constraint>([](const LineWrap &a,
+                                               const LineWrap &b) {
+        double d = a.Distance(b);
+        return d / (d + 300);
+    });
+
+    //function opposite to adjacent
+    //* returns double in range [0, inf), where inf is touching
+    auto far = make_shared<Constraint>([](const LineWrap & a,
+                                          const LineWrap & b) {
+        return 10.0 / a.Distance(b);
+    });
+
+    //function scoring adjacency between start of line a and line b
+    //* returns double in range [0, 1], where 0 is the best match
+    auto adjacentStart = make_shared<Constraint>([](const LineWrap & a,
+                                                    const LineWrap & b) {
+        auto d = b.Distance(a.start);
+        return d / (d+300.0);
+    });
+
+    //function scoring adjacency between end  of line a and line b
+    //* returns double in range [0, 1], where 0 is the best match
+    auto adjacentEnd = make_shared<Constraint>([](const LineWrap & a,
+                                                  const LineWrap & b) {
+        auto d = b.Distance(a.end);
+        return d / (d+300.0);
+    });
+
+    //function scoring adjacency between vertices of line a b
+    //* returns double in range [0, 1], where 0 is the best match
+    auto vertexAdjacent = make_shared<Constraint>([](const LineWrap & a,
+                                                     const LineWrap & b) {
+        auto a1 = b.Distance(a.start);
+        auto a2 = b.Distance(a.end);
+        a1 = a1 < a2 ? a1 : a2;
+
+        auto b1 = a.Distance(b.start);
+        auto b2 = a.Distance(b.end);
+        b1 = b1 < b2 ? b1 : b2;
+
+        a1 = a1 < b1 ? a1 : b1;
+
+        return a1 / (a1 + 300.0);
+    });
+
+    /*
+     * ELSD image processing
+     * detect segments and save them in separate image for testing purposes
+     */
+    //TODO make this work
+    auto samples = vector<vector<weak_ptr<LineWrap>>>{};
+
+    auto sampleFiles = vector<string>{/*"l1.PGM","l2.PGM","l3.PGM","l4.PGM"*/};
+    for (auto & sampleFile : sampleFiles) {
+        auto inFile = sampleFile;
+        inFile = "./" + inFile;
+        ImageInterface::Ptr image(new ElsdPgmFileReader(inFile));
+        ShapesDetectorInterface::Ptr detector(new ElsDetector);
+        detector->run(image);
+
+        string outFile = inFile + ".svg";
+        SvgWriterInterface::Ptr svg(new ElsdSvgWriter);
+        svg->setImageSize(image->xsize(), image->ysize());
+        const vector<LineSegment> &lines = detector->getLineSegments();
+        svg->addLineSegments(lines.begin(), lines.end());
+        ofstream ofs(outFile, ofstream::out);
+        ofs << *svg
+            << "</svg>";
+        ofs.close();
+
+        //Create vector of wrapped segments
+        auto segsShared = vector<shared_ptr<LineWrap>>();
+        for (const auto &line : lines)
+            segsShared.push_back(make_shared<LineWrap>(line));
+
+        auto segments = vector<weak_ptr<LineWrap>>();
+        for (const auto &line : segsShared)
+            segments.push_back(weak_ptr<LineWrap>(line));
+
+        samples.push_back(segments);
+    }
+
+    auto constraints = vector<shared_ptr<Constraint>>{adjacent, sizeMatch,
+                                                      parallel, perpendicular};
+
+    auto l00 = make_shared<LineWrap>(LineWrap{{0,0},{100,0}});
+    auto l01 = make_shared<LineWrap>(LineWrap{{100,100},{100,0}});
+    auto l02 = make_shared<LineWrap>(LineWrap{{0,0},{0,100}});
+    auto l03 = make_shared<LineWrap>(LineWrap{{0,100},{100,100}});
+    auto p0 = vector<weak_ptr<LineWrap>>{l00, l01, l02, l03};
+
+    auto l10 = make_shared<LineWrap>(LineWrap{{0,0},{500,0}});
+    auto l11 = make_shared<LineWrap>(LineWrap{{500,100},{500,0}});
+    auto l12 = make_shared<LineWrap>(LineWrap{{0,0},{0,100}});
+    auto l13 = make_shared<LineWrap>(LineWrap{{0,100},{500,100}});
+    auto p1 = vector<weak_ptr<LineWrap>>{l10, l11, l12, l13};
+
+    auto l20 = make_shared<LineWrap>(LineWrap{{0,0},{100,0}});
+    auto l21 = make_shared<LineWrap>(LineWrap{{120,97.9797},{100,0}});
+    auto l22 = make_shared<LineWrap>(LineWrap{{0,0},{20,97.9797}});
+    auto l23 = make_shared<LineWrap>(LineWrap{{20,97.9797},{120,97.9797}});
+    auto p2 = vector<weak_ptr<LineWrap>>{l20, l21, l22, l23};
+
+    auto l30 = make_shared<LineWrap>(LineWrap{{0,0},{100,0}});
+    auto l31 = make_shared<LineWrap>(LineWrap{{0,0},{0,100}});
+    auto l32 = make_shared<LineWrap>(LineWrap{{0,100},{100,0}});
+    auto l33 = make_shared<LineWrap>(LineWrap{{0,0},{50,50}});
+    auto n0 = vector<weak_ptr<LineWrap>>{l30, l31, l32, l33};
+
+    auto posSamples = vector<vector<weak_ptr<LineWrap>>>{p0, p1, p2};
+    auto negSamples = vector<vector<weak_ptr<LineWrap>>>{n0};
+
+    LearningSystem ls;
+    ls.GenerateModel(posSamples, negSamples, constraints);
 }
 
 //-----------------------------------------------------------------------------
